8a33045ed42eca1965e8e02515a8a6383b288358
kirill.grushin <kirgent@gmail.com>
2019-03-05 13:35:35 +0300

    auto commit

---
+++ remove_user_from_DB.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $(whoami) != "root" ]]; then
+echo "You must be root to run"
+exit 1
+fi
+
+if [[ -z "$1" ]]; then
+echo "No input login specified!"
+exit
+fi
+
+
+sudo -u postgres $(which psql) -U postgres <<SQL
+\c unidata
+SELECT id,login,email,first_name,last_name,created_at,created_by,email_notification FROM public.s_user;
+DELETE FROM public.s_password WHERE s_user_id = (SELECT id FROM public.s_user WHERE login = '$1');
+DELETE FROM public.s_user_property_value WHERE user_id = (SELECT id FROM public.s_user WHERE login = '$1');
+DELETE FROM public.s_user WHERE login = '$1';
+SELECT id,login,email,first_name,last_name,created_at,created_by,email_notification FROM public.s_user;
+SQL
